c8699f69245aa2fbcedbb17c9e6aa434bddada5e
Hector <rankecillo@gmail.com>
2024-10-25 16:22:55 -0600

    show msg, clear lines

--- chat-app/client.js
+++ chat-app/client.js
@@ -6,14 +6,45 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
 const socket = net.createConnection(
   { host: '127.0.0.1', port: 3008 },
   async () => {
     console.log('Connected to server');
 
-    const message = await rl.question('Enter a message > ');
+    const ask = async () => {
+      const message = await rl.question('Enter a message > ');
+      // move cursor 1 line up
+      await moveCursor(0, -1);
+      // clear current line where cursor is in
+      await clearLine(0);
 
-    socket.write(message);
+      socket.write(message);
+    };
+    ask();
+
+    socket.on('data', async (data) => {
+      await moveCursor();
+      await clearLine();
+      console.log(data.toString('utf-8'));
+
+      ask();
+    });
   }
 );
 
