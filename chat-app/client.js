520e0a4aa8f94daf6bc31099bd23e3444955415f
Hector <rankecillo@gmail.com>
2024-10-25 17:00:44 -0600

    added user id's

--- chat-app/client.js
+++ chat-app/client.js
@@ -6,14 +6,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const moveCursor = (dx, dy) => {
-  return new Promise((resolve, reject) => {
-    process.stdout.moveCursor(dx, dy, () => {
-      resolve();
-    });
-  });
-};
-
 const clearLine = (dir) => {
   return new Promise((resolve, reject) => {
     process.stdout.clearLine(dir, () => {
@@ -21,6 +13,15 @@ const clearLine = (dir) => {
     });
   });
 };
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+let id;
 
 const socket = net.createConnection(
   { host: '127.0.0.1', port: 3008 },
@@ -31,17 +32,34 @@ const socket = net.createConnection(
       const message = await rl.question('Enter a message > ');
       // move cursor 1 line up
       await moveCursor(0, -1);
+
       // clear current line where cursor is in
       await clearLine(0);
 
-      socket.write(message);
+      socket.write(`${id}-message-${message}`);
     };
     ask();
 
     socket.on('data', async (data) => {
-      await moveCursor();
-      await clearLine();
+      // log an empty line
+      console.log();
+
+      // move cursor 1 line up
+      await moveCursor(0, -1);
+
+      //clear line
+      await clearLine(0);
       console.log(data.toString('utf-8'));
+      if (data.toString('utf-8').substring(0, 2) === 'id') {
+        // when we are getting the id
+        // everything from the third characte until the end
+        id = data.toString('utf-8').substring(3);
+
+        console.log(`Your id is ${id}!\n`);
+      } else {
+        //When we are getting a message
+        //
+      }
 
       ask();
     });
